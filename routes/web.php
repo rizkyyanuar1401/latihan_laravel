0d0723b289dafea3c5d65d3f416f46b9525f87b0
hippedcheetah90 <47834825+hippedcheetah90@users.noreply.github.com>
2023-05-21 20:28:45 +0700

    Finished this Laravel Project as my exercise assignment in Fundamental Web Programming Class

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\StudentsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+
+Route::get('/', PagesController::class . '@home');
+Route::get('/about', PagesController::class . '@about');
+Route::get('/mahasiswa', MahasiswaController::class . '@index');
+
+// Route::get('/students', StudentsController::class . '@index');
+// Route::get('/students/create', StudentsController::class . '@create');
+// Route::get('/students/{student}', StudentsController::class . '@show');
+// Route::post('/students', StudentsController::class . '@store');
+// Route::delete('/students/{student}', StudentsController::class . '@destroy');
+// Route::get('/students/{student}/edit', StudentsController::class . '@edit');
+// Route::patch('/students/{student}', StudentsController::class . '@update');
+
+Route::resource('students', StudentsController::class);
